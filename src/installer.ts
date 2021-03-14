3b58f5df55d0c9775b5cc8ffdca7aeb7bfff00d1
Jim Verheijde <jim.verheijde@hotmail.com>
2021-03-14 14:47:45 +0100

    Upload install log as artifact

--- src/installer.ts
+++ src/installer.ts
@@ -3,6 +3,7 @@ import {SemVer} from 'semver'
 import {download} from './downloader'
 import * as core from '@actions/core'
 import {getOs, OSType} from './platform'
+import * as artifact from '@actions/artifact'
 
 export async function install(version: SemVer): Promise<void> {
   const executablePath: string = await download(version)
@@ -30,4 +31,20 @@ export async function install(version: SemVer): Promise<void> {
   core.debug(`Running install executable: ${executablePath}`)
   const exitCode = await exec(executablePath, installArgs, options)
   core.debug(`Installer exit code: ${exitCode}`)
+  if ((await getOs()) === OSType.linux) {
+    const artifactClient = artifact.create()
+    const artifactName = 'install-log'
+    const files = ['/tmp/cuda-installer.log']
+    const rootDirectory = '/tmp'
+    const artifactOptions = {
+      continueOnError: true
+    }
+    const uploadResult = await artifactClient.uploadArtifact(
+      artifactName,
+      files,
+      rootDirectory,
+      artifactOptions
+    )
+    core.debug(`Upload result: ${uploadResult}`)
+  }
 }
