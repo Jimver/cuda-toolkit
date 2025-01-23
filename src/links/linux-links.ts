d80ce875181f606ae1eebc9509fa977498b698f2
Johnny <johnnync13@gmail.com>
2025-01-23 22:34:34 +0100

    Update linux-links.ts

--- src/links/linux-links.ts
+++ src/links/linux-links.ts
@@ -13,6 +13,10 @@ export class LinuxLinks extends AbstractLinks {
     // Map of cuda SemVer version to download URL
     this.cudaVersionToURL = new Map([
       [
+        '12.8.0',
+        'https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda_12.8.0_570.86.10_linux.run'
+      ],
+      [
         '12.6.3',
         'https://developer.download.nvidia.com/compute/cuda/12.6.3/local_installers/cuda_12.6.3_560.35.05_linux.run'
       ],
