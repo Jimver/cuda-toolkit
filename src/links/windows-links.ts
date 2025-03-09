b8288d0219a536d8f22e06d5e14f8ecf3a0c3697
Johnny <johnnync13@gmail.com>
2025-03-09 15:47:22 +0100

    Update windows-links.ts

--- src/links/windows-links.ts
+++ src/links/windows-links.ts
@@ -25,6 +25,10 @@ export class WindowsLinks extends AbstractLinks {
 
   private cudaVersionToNetworkUrl: Map<string, string> = new Map([
     [
+      '12.8.1',
+      'https://developer.download.nvidia.com/compute/cuda/12.8.1/network_installers/cuda_12.8.1_windows_network.exe'
+    ],
+    [
       '12.8.0',
       'https://developer.download.nvidia.com/compute/cuda/12.8.0/network_installers/cuda_12.8.0_windows_network.exe'
     ],
